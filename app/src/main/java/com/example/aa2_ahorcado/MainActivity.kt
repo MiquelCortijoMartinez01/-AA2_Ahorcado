27199de025fcd2c3e1e65223d04d7ae1b07aea7b
Júlia <julia.martos@enti.cat>
2025-10-24 21:10:27 +0200

    ToolBar + exit done
    Está hecho lo del cambio de darck a light mode, solo falta adaptarlo para los colores que he metido yo a mano

--- app/src/main/java/com/example/aa2_ahorcado/MainActivity.kt
+++ app/src/main/java/com/example/aa2_ahorcado/MainActivity.kt
@@ -1,15 +1,49 @@
 package com.example.aa2_ahorcado
 
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.activity.ComponentActivity
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var myToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(myToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu:Menu?): Boolean {
+        menuInflater.inflate(R.menu.custom_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(item.itemId == R.id.dark_mode) {
+            // Codigo
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.applyDayNight()
+            true
+        } else if (item.itemId == R.id.light_mode) {
+            // Codigo
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.applyDayNight()
+            true
+        } else if (item.itemId == R.id.exit) {
+            finish()
+            true
+        }else {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
